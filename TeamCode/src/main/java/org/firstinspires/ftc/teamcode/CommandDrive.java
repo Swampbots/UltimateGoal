fcf15cd4bcf99b24abcb8c293d090fc12525cc28
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-12-05 19:34:02 -0500

    Updated TeleOp program with previous changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandDrive.java
@@ -6,17 +6,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpDriveControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpGripControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpIntakeControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpKickerControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpShooterControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpTransferControl;
-import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpWobbleControl;
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmControl;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Grip;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Kicker;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Shooter;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Transfer;
-import org.firstinspires.ftc.teamcode.robot.subsystems.WobbleArm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 
 @TeleOp(name = "Command Drive", group = "TeleOp")
 public class CommandDrive extends LinearOpMode implements DogeOpMode {
@@ -27,14 +29,16 @@ public class CommandDrive extends LinearOpMode implements DogeOpMode {
         Drive drive             = new Drive(hardwareMap,true);
         Kicker kicker           = new Kicker(hardwareMap);
         Shooter shooter         = new Shooter(hardwareMap);
-        WobbleArm wobble           = new WobbleArm(hardwareMap);
+        Arm arm                 = new Arm(hardwareMap);
+        Grip grip               = new Grip(hardwareMap);
         Intake intake           = new Intake(hardwareMap);
         Transfer transfer       = new Transfer(hardwareMap);
 
         commander.registerSubsystem(drive);
         commander.registerSubsystem(kicker);
         commander.registerSubsystem(shooter);
-        commander.registerSubsystem(wobble);
+        commander.registerSubsystem(arm);
+        commander.registerSubsystem(grip);
         commander.registerSubsystem(intake);
         commander.registerSubsystem(transfer);
 
@@ -45,7 +49,8 @@ public class CommandDrive extends LinearOpMode implements DogeOpMode {
                 new TeleOpDriveControl(drive,gamepad1),
                 new TeleOpKickerControl(kicker,gamepad1),
                 new TeleOpShooterControl(shooter,gamepad2),
-                new TeleOpWobbleControl(wobble,gamepad1,gamepad2),
+                new TeleOpArmControl(arm,gamepad1,gamepad2),
+                new TeleOpGripControl(grip,gamepad1),
                 new TeleOpIntakeControl(intake,gamepad2),
                 new TeleOpTransferControl(transfer,gamepad2)
         );
