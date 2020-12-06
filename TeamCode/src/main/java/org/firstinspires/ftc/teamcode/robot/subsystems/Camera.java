8419cb780c10b899563f0733d4d36cee69244d0d
BenRodkin <54512462+BenRodkin@users.noreply.github.com>
2020-12-05 20:30:17 -0500

    Added camera controls
    These are NOT finalized

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Camera.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import android.view.SubMenu;
+
+import com.disnodeteam.dogecommander.Subsystem;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class Camera extends OpenCvPipeline implements Subsystem {
+
+    @Override
+    public void initHardware() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
